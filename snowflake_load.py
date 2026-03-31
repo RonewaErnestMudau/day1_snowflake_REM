ad87c4f463a032c3246832d26337d760b9fb75ae
RonewaErnestMudau <Ronewa.Mudau@cci.digital>
2026-03-31 11:39:46 +0200

    correct cleaned dataset

--- snowflake_load.py
+++ snowflake_load.py
@@ -3,6 +3,7 @@ from sqlalchemy import create_engine
 from dotenv import load_dotenv 
 import os 
 import logging 
+from urllib.parse import quote_plus
 
 logging.basicConfig(level=logging.INFO, 
                     format='%(asctime)s | %(levelname)s | %(message)s') 
@@ -10,14 +11,33 @@ logger = logging.getLogger(__name__)
 load_dotenv() 
 
 # Load the cleaned dataset
-df = pd.read_csv('retail_orders_clean_ronewa_ernest.csv') 
-logger.info(f'Rows loaded from CSV: {len(df)}') 
+df = pd.read_csv('retail_orders_clean_ronewa_ernest_202603311107.csv') 
 
+#fixing date formats
+#df['order_date'] = pd.to_datetime(df['order_date'])
+logger.info(f'Rows loaded from CSV: {len(df)}') 
+"""
+print("USER:", os.getenv('SNOWFLAKE_USER'))
+print("ACCOUNT:", os.getenv('SNOWFLAKE_ACCOUNT'))
+
+conn_str = (
+    f"snowflake://{os.getenv('SNOWFLAKE_USER')}:{os.getenv('SNOWFLAKE_PASSWORD')}"
+    f"@{os.getenv('SNOWFLAKE_ACCOUNT')}/{os.getenv('SNOWFLAKE_DATABASE')}"
+    f"/{os.getenv('SNOWFLAKE_SCHEMA')}"
+    f"?warehouse={os.getenv('SNOWFLAKE_WAREHOUSE')}"
+)
+
+print("CONNECTION STRING:", conn_str)"""
 # Build Snowflake SQLAlchemy connection string 
-engine = create_engine( 
-    f"snowflake://{os.getenv('SNOWFLAKE_USER')}:{os.getenv('SNOWFLAKE_PASSWORD')}" 
-    f"@{os.getenv('SNOWFLAKE_ACCOUNT')}/{os.getenv('SNOWFLAKE_DATABASE')}" 
-    f"/{os.getenv('SNOWFLAKE_SCHEMA')}?warehouse={os.getenv('SNOWFLAKE_WAREHOUSE')}" )
+
+password = quote_plus(os.getenv('SNOWFLAKE_PASSWORD'))
+
+engine = create_engine(
+    f"snowflake://{os.getenv('SNOWFLAKE_USER')}:{password}"
+    f"@{os.getenv('SNOWFLAKE_ACCOUNT')}/{os.getenv('SNOWFLAKE_DATABASE')}"
+    f"/{os.getenv('SNOWFLAKE_SCHEMA')}"
+    f"?warehouse={os.getenv('SNOWFLAKE_WAREHOUSE')}"
+)
 
 # Load DataFrame to Snowflake 
 df.to_sql( 
--- snowflake_query.py
+++ snowflake_query.py
@@ -3,16 +3,20 @@ from sqlalchemy import create_engine
 from dotenv import load_dotenv 
 import os 
 import logging 
+from urllib.parse import quote_plus
 
 logging.basicConfig(level=logging.INFO) 
 logger = logging.getLogger(__name__) 
 load_dotenv() 
 
-engine = create_enginee( 
-    f"snowflake://{os.getenv('SNOWFLAKE_USER')}:{os.getenv('SNOWFLAKE_PASSWORD')}"
-    f"@{os.getenv('SNOWFLAKE_ACCOUNT')}/{os.getenv('SNOWFLAKE_DATABASE')}" 
-    f"/{os.getenv('SNOWFLAKE_SCHEMA')}?warehouse={os.getenv('SNOWFLAKE_WAREHOUSE')}" 
-    ) 
+password = quote_plus(os.getenv('SNOWFLAKE_PASSWORD'))
+
+engine = create_engine(
+    f"snowflake://{os.getenv('SNOWFLAKE_USER')}:{password}"
+    f"@{os.getenv('SNOWFLAKE_ACCOUNT')}/{os.getenv('SNOWFLAKE_DATABASE')}"
+    f"/{os.getenv('SNOWFLAKE_SCHEMA')}"
+    f"?warehouse={os.getenv('SNOWFLAKE_WAREHOUSE')}"
+) 
 # Query 1: Revenue by region 
 df_region = pd.read_sql("""
      SELECT region, 
@@ -24,16 +28,21 @@ df_region = pd.read_sql("""
 """, engine) 
 
 logger.info("Revenue by Region:") 
-print(df_region) 
-# Query 2: Monthly revenue trend 
-df_monthly = pd.read_sql(""" 
-    SELECT DATE_TRUNC('month', order_date) AS month, 
-        SUM(revenue) AS revenue FROM retail_orders 
-    GROUP BY month ORDER BY month 
-""", engine) 
+print(df_region)
 
-logger.info("Monthly Revenue:") 
-print(df_monthly) 
+# Query 2: Monthly revenue trend 
+
+df_monthly = pd.read_sql("""
+    SELECT
+        DATE_TRUNC('month', TO_DATE(order_date)) AS month,
+        SUM(revenue) AS revenue
+    FROM retail_orders
+    GROUP BY DATE_TRUNC('month', TO_DATE(order_date))
+    ORDER BY month
+""", engine)
+logger.info("Monthly Revenue:")
+print(df_monthly)
+
 # Query 3: Top 5 customers (stretch) 
 # 
 df_top = pd.read_sql(""" 
